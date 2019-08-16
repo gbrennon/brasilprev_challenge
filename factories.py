3fb4e0fa68dbc85884e922380911863461311e85
gbrennon <glauberbrennon@gmail.com>
2019-08-16 19:30:38 -0300

    Added property class, factory and tests

--- factories.py
+++ factories.py
@@ -1,6 +1,8 @@
+from random import randint
 from factory import Factory, Faker, fuzzy
 
 from player import Player, Behavior
+from property import Property, SALE_BOUNDARIES, RENTAL_BOUNDARIES
 
 
 class PlayerFactory(Factory):
@@ -9,3 +11,11 @@ class PlayerFactory(Factory):
 
     name = Faker('name')
     behavior = fuzzy.FuzzyChoice(Behavior)
+
+
+class PropertyFactory(Factory):
+    class Meta:
+        model = Property
+
+    sale_price = fuzzy.FuzzyAttribute(lambda: randint(*SALE_BOUNDARIES))
+    rental_price = fuzzy.FuzzyAttribute(lambda: randint(*RENTAL_BOUNDARIES))
---
+++ tests/test_property.py
@@ -0,0 +1,24 @@
+import unittest
+
+from factories import PropertyFactory, SALE_BOUNDARIES, RENTAL_BOUNDARIES
+
+class PropertyTestCases(unittest.TestCase):
+    def test_property_sale_price_in_boundaries(self):
+        try:
+            p = PropertyFactory(sale_price=SALE_BOUNDARIES[1])
+        except ValueError:
+            self.fail('Property sale_price raised ValueError unexpectedly!')
+
+    def test_property_rental_price_in_boundaries(self):
+        try:
+            p = PropertyFactory(rental_price=RENTAL_BOUNDARIES[1])
+        except ValueError:
+            self.fail('Property rental_price raised ValueError unexpectedly!')
+
+    def test_property_sale_price_out_of_boundaries(self):
+        with self.assertRaises(ValueError):
+            PropertyFactory(sale_price=SALE_BOUNDARIES[1]+1)
+
+    def test_property_rental_price_not_in_boundary(self):
+        with self.assertRaises(ValueError):
+            PropertyFactory(rental_price=RENTAL_BOUNDARIES[1]+1)
